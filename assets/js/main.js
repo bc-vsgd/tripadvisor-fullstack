50e937af7e57160ba7fa40345bf07b3e43d3086d
Charles Benoît <charlesbenoit@MacBook-Pro-de-Charles.local>
2023-12-13 22:55:07 +0100

    Added opacity behind modal and banner fixed at the bottom of the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,36 @@ const serverUrl = "https://site--tripadvisor-backend--r6xgg7xm7vcz.code.run";
 // const serverUrl = "http://localhost:3000";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // console.log("Listener doc");
-
+  // Display modal
   document.querySelector("#connection-button").addEventListener("click", () => {
-    // console.log("Connectez-vous");
-    document.querySelector(".modal").classList.toggle("hidden");
+    console.log("Connexion");
+    document.querySelector(".modal-div").classList.toggle("hidden");
   });
-  document.querySelector(".modal").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    // console.log("submit listener");
 
-    // Retrieve form values
-    const user = {
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      email: document.querySelector("#email").value,
-      message: document.querySelector("#message").value,
-    };
-    document.querySelector(".modal").classList.toggle("hidden");
-    const response = await axios.post(`${serverUrl}/form`, user);
-    console.log(`${serverUrl}/form`);
-    console.log(response.data);
+  // Submit event
+  document
+    .querySelector(".modal form")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      // Retrieve form values
+      const user = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      };
+      // Close modal
+      document.querySelector(".modal-div").classList.toggle("hidden");
+      // Send data to server
+      const response = await axios.post(`${serverUrl}/form`, user);
+      console.log(`${serverUrl}/form`);
+      console.log(response.data);
+    });
+
+  //Close modal
+  document.querySelector(".modal .icon-times").addEventListener("click", () => {
+    console.log("Fermer");
+    document.querySelector(".modal-div").classList.toggle("hidden");
   });
+  // Dernière accolade + parenthèse
 });
